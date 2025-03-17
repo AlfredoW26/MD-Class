385aa8fb3fefeed6916db9cfbaab7c00f2e59c51
AlfredoW26 <147214013+AlfredoW26@users.noreply.github.com>
2025-03-17 14:17:57 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ def main():
   st.info('This app using machine learning')
 
   # input data by user
-  erythema = lst.slider('Erythema', min_value = 0, max_value = 3, value = 2)
+  erythema = st.slider('Erythema', min_value = 0, max_value = 3, value = 2)
 
 if __name__ == "__main__":
   main()
